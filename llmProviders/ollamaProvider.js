307eaac2b8be314d3410fd2a582db650d3526aa3
aleksaelezovic <aleksaelezovic@gmail.com>
2025-04-04 12:04:17 +0200

    add ollama llmProvider

---
+++ llmProviders/ollamaProvider.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { LLMProvider } from "./llmProvider.js";
+import { PassThrough, Transform } from "stream";
+
+export class OllamaProvider extends LLMProvider {
+  constructor(config) {
+    super(config);
+  }
+
+  async createChatCompletion(prompt, stream = false) {
+    const response = await axios
+      .post(
+        "http://localhost:11434/api/generate",
+        {
+          model: this.model,
+          prompt,
+          stream,
+        },
+        { responseType: stream ? "stream" : "json" }
+      )
+      .then((r) => r.data);
+    if (!stream) return response?.response ?? "";
+
+    const responseStream = new PassThrough({ objectMode: true });
+    const t = new TextDecoder();
+    response
+      .pipe(
+        new Transform({
+          transform(chunk, _, callback) {
+            const text = t.decode(chunk);
+            const content = JSON.parse(text).response;
+            this.push({ choices: [{ delta: { content } }] });
+            callback();
+          },
+        })
+      )
+      .pipe(responseStream);
+    return responseStream;
+  }
+}
